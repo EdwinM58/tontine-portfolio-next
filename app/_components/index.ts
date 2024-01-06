2b73a9ee1564c7fe1bb1a4de9c8b9f55223a0534
EdwinM58 <121357284+EdwinM58@users.noreply.github.com>
2024-01-06 15:25:41 -0500

    Delete app directory

--- app/_components/index.ts
+++
@@ -1,9 +0,0 @@
-import Experience from './Experience'
-import Explore from './Explore'
-import Footer from './Footer'
-import Header from './Header'
-import Hero from './Hero'
-import Services from './Services'
-import Work from './Work'
-
-export {Experience, Explore, Footer, Header, Hero, Services, Work}

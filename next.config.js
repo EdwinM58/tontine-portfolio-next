eee0401e318fe582bcdb2eae55ff4349177fbac8
EdwinM58 <121357284+EdwinM58@users.noreply.github.com>
2024-01-06 15:38:08 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "loremflickr.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.dummyjson.com",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+      },
+    ],
+  },
   env: {
     projectId: "h9ejl8k0",
     dataset: "production",

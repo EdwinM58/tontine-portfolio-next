6d3df496e539e079acc5d927c724eb3d79041ae0
EdwinM58 <121357284+EdwinM58@users.noreply.github.com>
2024-01-06 03:41:55 -0500

    Add files via upload

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="mt-auto w-screen px-4 py-2 h-28 flex flex-col bg-[#E7E7E7] ">
+    <div className=" bg-[#E7E7E7]  w-screen bottom-0 left-0 h-32 p-4">
       <h1 className="font-bold">Let&apos;s connect!</h1>
       <Link href="https://mail.google.com/">edwin.mongare@gmail.com</Link>
       <p>+1(201)-235-6334</p>
@@ -12,3 +12,10 @@ const Footer = () => {
 };
 
 export default Footer;
+{
+  /* <div className="mt-auto w-screen px-4 py-2 h-28 flex flex-col bg-[#E7E7E7] absolute">
+<h1 className="font-bold">Let&apos;s connect!</h1>
+<Link href="https://mail.google.com/">edwin.mongare@gmail.com</Link>
+<p>+1(201)-235-6334</p>
+</div> */
+}

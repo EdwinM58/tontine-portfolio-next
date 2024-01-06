2ae63e6887bb3b2486cf6b3a388906081bcbb63b
EdwinM58 <121357284+EdwinM58@users.noreply.github.com>
2024-01-06 15:08:08 -0500

    Add files via upload

---
+++ myprojects/page.tsx
@@ -0,0 +1,93 @@
+import Link from "next/link";
+import Image from "next/image";
+import projects from "../db.json";
+
+const MyProjectsPage = () => {
+  return (
+    <div className="">
+      <div className="flex flex-col sm:flex-row sm:space-y-0 space-y-8 justify-between px-8 sm:px-32 py-16">
+        <h1 className="text-6xl font-bold">My Projects</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur incididunt ut labore et um
+          <br />
+          dolor sit amet, consectetur incididunt ut labore et
+        </p>
+      </div>
+
+      <div className="flex flex-wrap justify-center">
+        {projects.map((e, i) => (
+          <ProjectCard
+            key={e.id}
+            title={e.title}
+            description={e.description}
+            imgUrl={e.image}
+            stack={e.stack}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const ProjectCard = ({
+  title,
+  description,
+  imgUrl,
+  stack,
+}: ProjectCardProps) => {
+  return (
+    <div className="p-6 flex flex-col gap-y-2 mx-auto max-w-full rounded-md drop-shadow-lg">
+      <div className="max-w-full flex-col md:flex-row rounded-md bg-gray-300 flex mt-8 md:w-[450px] h-96 justify-between">
+        <div className="relative flex flex-col p-4 rounded-md drop-shadow-md space-y-4 ">
+          <div className="flex justify-center ">
+            <Image
+              src={imgUrl}
+              alt="project"
+              width={250}
+              height={200}
+              className="rounded-md "
+            />
+          </div>
+
+          <h1 className="text-xl font-bold">{title}</h1>
+          <p className="text-sm">{description}</p>
+          <div className="relative flex space-x-4 justify-end">
+            {stack.map((e) => (
+              <p className="font-bold" key={e}>
+                * {e}
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center border-2 w-16 rounded-3xl">
+        <Link href={"https://github.com/"}>github</Link>
+      </div>
+    </div>
+  );
+};
+
+export default MyProjectsPage;
+
+{
+  /* <div className="max-w-full flex-col md:flex-row rounded-md bg-gray-300 flex mt-8 md:w-[450px] h-96 justify-between">
+<div className="relative flex flex-col p-4 rounded-md drop-shadow-md space-y-4 ">
+  <Image
+    src={imgUrl}
+    alt="project"
+    width={250}
+    height={200}
+    className="rounded-md "
+  />
+  <h1 className="text-xl font-bold">{title}</h1>
+  <p className="text-sm">{description}</p>
+  <div className="relative flex space-x-4 justify-end">
+    {stack.map((e) => (
+      <p className="font-bold" key={e}>
+        * {e}
+      </p>
+    ))}
+  </div>
+</div>
+</div> */
+}
---
+++ mystore/page.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import React, {
+  useMemo,
+  useEffect,
+  useState,
+  FormEvent,
+  MouseEventHandler,
+} from "react";
+// import products from "../db.json";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import Search from "../_components/Search";
+
+const MyStorePage = () => {
+  const [hovered, setHovered] = useState<boolean>(true);
+  const [query, setQuery] = useState<string>("");
+  const [sortOrder, setSortOrder] = useState<string>("lowToHigh");
+  const [products, setProducts] = useState<Products[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://dummyjson.com/products");
+      const data = await response.json();
+
+      setProducts(data.products);
+    };
+
+    fetchData();
+  }, []);
+
+  const filteredProducts = useMemo(() => {
+    return products.filter(
+      (product) =>
+        product.title.toLowerCase().includes(query.toLowerCase()) ||
+        product.description.toLowerCase().includes(query.toLowerCase()) ||
+        product.brand.toLowerCase().includes(query.toLowerCase()) ||
+        product.category.toLowerCase().includes(query.toLowerCase())
+    );
+  }, [query, products]);
+
+  const sortedProducts = useMemo(() => {
+    let sorted = [...filteredProducts];
+    if (sortOrder === "lowToHigh") {
+      sorted.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+    } else {
+      sorted.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
+    }
+    return sorted;
+  }, [filteredProducts, sortOrder]);
+
+  const handleQuery = (q: string) => {
+    setQuery(q);
+  };
+
+  const handleOrder = (o: string) => {
+    setSortOrder(o);
+  };
+
+  return (
+    <div className="relative flex flex-col">
+      <div className="sm:flex sm:justify-between space-y-4 p-4">
+        <h1 className="sm:text-6xl text-4xl font-bold flex justify-start items-center">
+          My Products
+        </h1>
+        <Search
+          query={query}
+          setQuery={handleQuery}
+          sortOrder={sortOrder}
+          setSortOrder={handleOrder}
+        />
+      </div>
+
+      <div className="flex flex-wrap justify-center mt-12 mb-24">
+        {sortedProducts.map((e, i) => (
+          <ProductCard
+            key={e.id}
+            name={e.title}
+            description={e.description}
+            price={e.price}
+            imgUrl={
+              e.images[i] == undefined
+                ? "https://upload.wikimedia.org/wikipedia/commons/c/ce/Cybertruck-fremont-cropped.jpg"
+                : e.images[i]
+            }
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const ProductCard = ({
+  name,
+  description,
+  price,
+  imgUrl,
+}: ProductCardProps) => {
+  console.log(imgUrl);
+
+  return (
+    <div className=" drop-shadow-lg rounded-xl mx-auto bg-gray-300 flex flex-col justify-end w-64 h-[500px] mt-8">
+      <Image
+        src={imgUrl}
+        height={300}
+        width={300}
+        alt="product"
+        className="rounded-t-xl relative w-auto h-auto "
+      />
+      <div className=" flex flex-col justify-end w-64 h-[500px] mt-8">
+        <div className="p-2  rounded-b-xl">
+          <h1 className="text-l font-bold">{name}</h1>
+          <p>{description}</p>
+          <p className="mt-4 font-bold">
+            <span className="text-green-700">$ </span>
+            {price}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyStorePage;
